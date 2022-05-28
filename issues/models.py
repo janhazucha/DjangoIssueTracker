57ce8a4ef57a5ac239759e3332bdcca3ae54c260
janhazucha <honzahazucha7@gmail.com>
2022-05-28 20:04:23 +0200

    Issues view and templates

--- issues/models.py
+++ issues/models.py
@@ -4,19 +4,23 @@ from .utils import ChoiceEnum
 
 
 class Issue(models.Model):
+    CATEGORY_CHOICES = (
+        ('Bug', 'Bug'),
+        ('Doc', 'Documentation'),
+        ('Imp', 'Improvement')
+    )
+
+    STATUS_CHOICES = (
+        ('ToDo', 'ToDo'),
+        ('Proc', 'Processing'),
+        ('Done', 'Done')
+    )
+
     title = models.CharField(max_length=120)
     description = models.TextField()
     author = models.ForeignKey(User, related_name='issue_author', on_delete=models.CASCADE)
     assignee = models.ForeignKey(User, related_name='issue_assignee', on_delete=models.CASCADE, null=True)
+    category = models.CharField(choices=CATEGORY_CHOICES, max_length=100)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-    class Status(ChoiceEnum):
-        TODO = 'todo'
-        PROCESSING = 'processing'
-        DONE = 'done'
-
-    class Category(ChoiceEnum):
-        BUG = 'bug'
-        DOCUMENTATION = 'documentation'
-        IMPROVEMENT = 'improvement'
--- issues/views.py
+++ issues/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .models import Issue
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+def get_issues(request):
+
+    list_of_issues = Issue.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(list_of_issues, 20)
+    try:
+        issues = paginator.page(page)
+
+    except PageNotAnInteger:
+
+        issues = paginator.page(1)
+
+    except EmptyPage:
+
+        issues = paginator.page(paginator.num_pages)
+
+    return render(request, "issues.html", {'issues': issues})
