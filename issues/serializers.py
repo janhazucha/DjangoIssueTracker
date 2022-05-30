586c0437c199a0791bf970ad062bb6639da1e5de
janhazucha <honzahazucha7@gmail.com>
2022-05-30 22:05:56 +0200

    status added to serializers

--- issues/serializers.py
+++ issues/serializers.py
@@ -1,11 +1,12 @@
-from rest_framework.serializers import ModelSerializer,SerializerMethodField
+from rest_framework.serializers import ModelSerializer, SerializerMethodField
 from .models import Issue
 
 
 class IssueSerializer(ModelSerializer):
     class Meta:
         model = Issue
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'category', 'status', 'author_username',
+                  'assignee_username', 'spent_time']
 
     spent_time = SerializerMethodField()
 
@@ -13,5 +14,11 @@ class IssueSerializer(ModelSerializer):
         return obj.get_spent_time()
 
     author_username = SerializerMethodField()
+
     def get_author_username(self, obj):
         return obj.author.username
+
+    assignee_username = SerializerMethodField()
+
+    def get_assignee_username(self, obj):
+        return obj.assignee.username
