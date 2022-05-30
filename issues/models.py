e16b9d515451f16be843bb8ebedee5471433fe9f
janhazucha <honzahazucha7@gmail.com>
2022-05-30 16:41:39 +0200

    added spent_time and usernames into serializers

--- issues/models.py
+++ issues/models.py
@@ -1,27 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Issue(models.Model):
     CATEGORY_CHOICES = (
         ('Bug', 'Bug'),
-        ('Doc', 'Documentation'),
-        ('Imp', 'Improvement')
+        ('Documentation', 'Documentation'),
+        ('Improvement', 'Improvement')
     )
 
     STATUS_CHOICES = (
-        ('ToDo', 'ToDo'),
-        ('Proc', 'Processing'),
+        ('Todo', 'Todo'),
+        ('Processing', 'Processing'),
         ('Done', 'Done')
     )
 
     title = models.CharField(max_length=100)
     description = models.TextField(null=True)
     author = models.ForeignKey(User, related_name='issue_author', on_delete=models.CASCADE)
-    assignee = models.ForeignKey(User, related_name='issue_assignee', on_delete=models.CASCADE, null=False)
+    assignee = models.ForeignKey(User, related_name='issue_assignee', on_delete=models.CASCADE, null=True)
     category = models.CharField(choices=CATEGORY_CHOICES, max_length=100, default='Bug')
-    status = models.CharField(choices=STATUS_CHOICES, max_length=50, default='ToDo')
-    created_at = models.DateField(auto_now_add=True)
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50, default='Todo')
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
+
+    def get_spent_time(self):
+        delta = (self.updated_at - self.created_at).total_seconds()//60
+        return delta
--- issues/serializers.py
+++ issues/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,SerializerMethodField
 from .models import Issue
 
 
@@ -6,3 +6,12 @@ class IssueSerializer(ModelSerializer):
     class Meta:
         model = Issue
         fields = '__all__'
+
+    spent_time = SerializerMethodField()
+
+    def get_spent_time(self, obj):
+        return obj.get_spent_time()
+
+    author_username = SerializerMethodField()
+    def get_author_username(self, obj):
+        return obj.author.username
