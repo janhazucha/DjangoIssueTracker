724e93e783c285133678b4d504a17b66063e5495
janhazucha <honzahazucha7@gmail.com>
2022-05-30 12:44:07 +0200

    Edit button in issues list and redirect fixed

--- issues/urls.py
+++ issues/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import get_issues, create_issue, rest_issues, get_issue_by_id_rest
+from .views import get_issues, create_issue, edit_issue, get_issues_rest, get_issue_by_id_rest
 
 urlpatterns = [
-    path('', get_issues, name='issues'),
+    path('', get_issues, name='get_issues'),
     path('create', create_issue, name='create_issue'),
+    path('edit/<int:issue_id>', edit_issue, name='edit_issue'),
 
     #REST API
-    path('getIssues', rest_issues, name='get_issues'),
+    path('get_issues_rest', get_issues_rest, name='get_issues_rest'),
     path('<int:id>', get_issue_by_id_rest, name='get_issue_by_id_rest'),
 
 ]
--- issues/views.py
+++ issues/views.py
@@ -1,17 +1,18 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import Issue
 from .forms import IssueForm
 
-import json
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponseRedirect
 from .serializers import IssueSerializer
 
 
 def get_issues(request):
-    issues_list = Issue.objects.all()
+    issues_list = Issue.objects.all().order_by('-created_at')
     return render(request, "issues.html", {'issues_list': issues_list})
 
 
@@ -23,13 +24,27 @@ def create_issue(request):
         form = IssueForm(request.POST)
         if form.is_valid():
             form.save()
-
+            return HttpResponseRedirect(reverse('get_issues'))
     context = {'form': form}
     return render(request, "issueform.html", context)
 
+def edit_issue(request, issue_id):
+    issue = Issue.objects.get(id=issue_id)
+    if request.method != 'POST':
+        form = IssueForm(instance=issue)
+    else:
+        form = IssueForm(instance=issue, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('get_issues'))
+    context = {'issue': issue, 'form': form}
+    return render(request, 'issueform.html', context)
+
+
+
 
 @api_view(['GET'])
-def rest_issues(request):
+def get_issues_rest(request):
     issues_set = Issue.objects.all()
     serializer = IssueSerializer(issues_set, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
